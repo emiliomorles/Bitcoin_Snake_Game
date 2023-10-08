7272dbfb27be8b261374ea7e8b617b0bb4ddc0ce
Emilio Morles <emiliomorles>
2023-10-08 09:32:53 -0600

    v.1.0

---
+++ main.py
@@ -0,0 +1,47 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game by @Emilio_Morles")
+screen.tracer(0)  # https://docs.python.org/3.3/library/turtle.html?highlight=turtle#turtle.tracer
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:  # It detects collision with food
+        food.refresh()  # The food appears in another random place everytime the snake eats it.
+        # snake.create_snake()
+        scoreboard.increase_score()
+        snake.extend()
+
+    if snake.head.xcor() > 295 or snake.head.ycor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() < -295:
+        # It detects collision with a wall.
+        scoreboard.game_over()
+        game_is_on = False
+
+    pass
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:  # It detects collision with its own tail.
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
